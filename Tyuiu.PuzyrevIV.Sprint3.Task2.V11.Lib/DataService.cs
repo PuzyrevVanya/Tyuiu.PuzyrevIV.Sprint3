d14458ce663bbf19ce3d0dabe171f58c17118b61
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-21 15:25:16 +0500

    Task2

--- Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Lib/DataService.cs
+++ Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Lib/DataService.cs
@@ -6,20 +6,23 @@ using System.Threading.Tasks;
 
 using tyuiu.cources.programming.interfaces.Sprint3;
 
-namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Lib
+namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V14.Lib
 {
-    public class DataService : ISprint3Task2V11
+    public class DataService : ISprint3Task2V14
     {
-        public double GetSumSeries(double value, int startValue, int stopValue)
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
         {
             double SumSeries = 0;
-            int k = startValue;
+
             do
             {
+                int k = startValue;
+
                 SumSeries = SumSeries + (value / k * Math.Sqrt(3));
                 startValue++;
             } while (startValue <= stopValue);
+
             return Math.Round(SumSeries, 3);
-        }   
+        }
     }
-}
+} 
--- Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Test/DataServiceTest.cs
+++ Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Test/DataServiceTest.cs
@@ -1,23 +1,23 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-using Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Lib;
+using Tyuiu.PuzyrevIV.Sprint3.Task2.V14.Lib;
 
-namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Test
+namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V14.Test
 {
     [TestClass]
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void ValidGetSumSeries()
+        public void ValidGetMultiplySeries()
         {
             DataService ds = new DataService();
 
-            double value = 5;
+            int value = 5;
             int startValue = 1;
             int stopValue = 6;
 
-            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
 
             double wait = 51.962;
 
