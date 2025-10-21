60d72fbdc771b21e2492451831371ad5e386d2c9
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-21 15:28:25 +0500

    Task2

---
+++ Tyuiu.PuzyrevIV.Sprint3.Task2.V14.Lib/DataService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V14.Lib
+{
+    public class DataService : ISprint3Task2V14
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double SumSeries = 0;
+
+            do
+            {
+                int k = startValue;
+
+                SumSeries = SumSeries + (value / k * Math.Sqrt(3));
+                startValue++;
+            } while (startValue <= stopValue);
+
+            return Math.Round(SumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.PuzyrevIV.Sprint3.Task2.V14/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PuzyrevIV.Sprint3.Task2.V14.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Тема: Sprint #3. Оператор цикла do-while.                               *");
+            Console.WriteLine("* Задание #2. Вариант #14.                                                *");
+            Console.WriteLine("* Выполнил: Пузырев И.В. | СМАРТБ 25 - 1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая используя цикл do...while, которая вычисляет*");
+            Console.WriteLine("* произведение ряда по формуле, при x = 5 П = = (x / k)*k*3               *");
+            Console.WriteLine("* (k от 1 до 6)                                                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int value = 5;
+            int startValue = 1;
+            int stopValue = 6;
+
+            Console.WriteLine("Переменая Х = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма рада = " + ds.GetMultiplySeries(value, startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
