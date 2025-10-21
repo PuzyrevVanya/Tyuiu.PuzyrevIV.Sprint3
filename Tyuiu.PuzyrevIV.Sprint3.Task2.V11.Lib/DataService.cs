646b1c83d50284e3175c68d87a65f3842f306486
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-21 15:18:56 +0500

    Task2

---
+++ Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Lib
+{
+    public class DataService : ISprint3Task2V11
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double SumSeries = 0;
+            int k = startValue;
+            
+            do
+            {
+                SumSeries = SumSeries + (value / k * Math.Sqrt(3));
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(SumSeries, 3);
+        }   
+    }
+}
---
+++ Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 5;
+            int startValue = 1;
+            int stopValue = 6;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = 51.962;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
