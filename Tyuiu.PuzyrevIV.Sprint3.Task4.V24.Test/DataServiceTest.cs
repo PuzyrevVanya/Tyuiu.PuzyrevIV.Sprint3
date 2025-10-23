10f87f8048148c26f4a76a700ab2e6c98deec721
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-23 16:25:15 +0500

    Task4

---
+++ Tyuiu.PuzyrevIV.Sprint3.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PuzyrevIV.Sprint3.Task4.V24.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task4.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int startValue = 5;
+            int stopValue = -5;
+
+            double res = ds.Calculate(startValue, stopValue);
+
+            double wait = 0;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PuzyrevIV.Sprint3.Task4.V24/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PuzyrevIV.Sprint3.Task4.V24.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task4.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Тема: Sprint #3. Использование операторов continue и break в циклах.    *");
+            Console.WriteLine("* Задание #4. Вариант #24.                                                *");
+            Console.WriteLine("* Выполнил: Пузырев И.В. | СМАРТБ 25 - 1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=(sin(x)/x)+2 При х = 0 прервать цикл.                         *");
+            Console.WriteLine("* Полученные значения перемножать.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 5;
+            int stopValue = -5;
+            
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество символов = " + ds.Calculate(startValue, stopValue));
+
+            Console.ReadKey();
+        }
+    }
+}
