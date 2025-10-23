4f8272cd59ea469bbaf3dffc1b3e1eac1978f0c0
Ivan Puzyrev <Tyuiu.PuzyrevIV@outlook.com>
2025-10-23 15:43:21 +0500

    Task3

---
+++ Tyuiu.PuzyrevIV.Sprint3.Task3.V16.Test/DataService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PuzyrevIV.Sprint3.Task3.V16.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task3.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCharCount()
+        {
+            DataService ds = new DataService();
+
+            string str = "ice nice ice creamcc";
+            char chr = 'c';
+
+            double res = ds.GetCharCount(str, chr);
+
+            double wait = 6;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.PuzyrevIV.Sprint3.Task3.V16/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PuzyrevIV.Sprint3.Task3.V16.Lib;
+
+namespace Tyuiu.PuzyrevIV.Sprint3.Task3.V16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Тема: Sprint #3. Оператор цикла foreach.                                *");
+            Console.WriteLine("* Задание #3. Вариант #11.                                                *");
+            Console.WriteLine("* Выполнил: Пузырев И.В. | СМАРТБ 25 - 1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach подсчитать количество букв “c”                   *");
+            Console.WriteLine("* в строке ice nice ice creamcc                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "ice nice ice creamcc";
+            char chr = 'c';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Исхомый символ = " + chr);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Количество символов = " + ds.GetCharCount(value, chr));
+
+            Console.ReadKey();
+        }
+    }
+}
